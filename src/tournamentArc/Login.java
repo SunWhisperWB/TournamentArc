52332189f445e7775aedc002d27c05a04d959090
SunWhisperWB <hugojosemoreiramonteiro@gmail.com>
2021-02-24 09:59:04 +0000

    added Register from other projects and refactored

--- src/tournamentArc/Login.java
+++ src/tournamentArc/Login.java
@@ -1,2 +1,51 @@
-package tournamentArc;public class Login {
+package tournamentArc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/logIn"})
+public class Login extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8"); //setting charset
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException { //processing resquest
+        processRequest(request, response);
+    }
+
+    //making new login on the web app
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        PrintWriter out = response.getWriter();
+        CreateLogin lg = new CreateLogin();
+
+        String name = request.getParameter("username");
+        String pwd = request.getParameter("password");
+
+        if(lg.validaLogin(name, pwd)){
+            request.getRequestDispatcher("loja.jsp").forward(request, response);
+        }else{
+            out.print("Error Logging in, Password or Username Incoorrect");
+            request.getRequestDispatcher("erro.jsp").forward(request, response);
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
---
+++ src/tournamentArc/Register.java
@@ -0,0 +1,74 @@
+package tournamentArc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.sql.PreparedStatement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/Register"})
+public class Register extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("username");
+        String pwd = request.getParameter("password");
+        try{
+            Connector objCon = new Connector();
+            objCon.openConnection();
+
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte []messageDigest = md.digest(pwd.getBytes("UTF-8"));
+
+            StringBuilder sb = new StringBuilder();
+
+            for(byte b : messageDigest){
+                sb.append(String.format("%02x", 0xFF & b));
+            }
+
+            pwd = sb.toString();
+
+            PreparedStatement ps = objCon.conn.prepareStatement("INSERT INTO utilizador(id, nome, pwd) VALUES (?,?,?)");
+            ps.setString(1, null);
+            ps.setString(2, name);
+            ps.setString(3, pwd);
+
+            int i = ps.executeUpdate();
+
+            if(i>0){
+                request.setAttribute("entrou", "entrou");
+                request.getRequestDispatcher("loja.jsp").forward(request, response);
+            }
+
+        }catch(Exception e){
+            out.print(e);
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
